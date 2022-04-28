9d421b78ca6f58627a7587cf07379f5d04dded79
anup <anuppal602@gmail.com>
2022-04-28 07:12:05 +0530

    topbar

---
+++ src/topbar/Topbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './topbar.css'
+function Topbar() {
+  return (
+    <div className="top">
+        <div className="topLeft">
+        <i class="fa-brands fa-facebook"></i>
+        <i class="fa-brands fa-twitter"></i>
+        </div>
+        <div className="topRight">right</div>
+        <div className="topcenter"></div>
+    </div>
+  )
+}
+
+export default Topbar
